936d7c515d05abc4d19fc00f472cc7b5b944eda7
Wych <wychwitchcraft@gmail.com>
2022-05-10 22:47:19 -0500

    OOP!!! ON DONM!!

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+const temp1 = [17, 21, 23];
+const temp2 = [12, 5, -5, 0, 4];
+const printForecast = function (temps) {
+  let printStr = "...";
+  for (let i = 0; i < temps.length; i++) {
+    printStr += `${temps[i]}ºC in ${i + 1} days...`;
+  }
+  console.log(printStr);
+};
+printForecast(temp1);
 
+printForecast(temp2);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,70 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+const defaultScore = 20;
+
+let score = defaultScore;
+
+let highScore = 0;
+
+let alive = true;
+
+const changeMessage = function (text) {
+  document.querySelector('.message').textContent = text;
+};
+
+const changeBG = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+const initGame = function () {
+  score = defaultScore;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  changeBG('#222');
+  document.querySelector('.number').style.width = '15rem';
+  changeMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  alive = true;
+  console.log('started game');
+};
+
+initGame();
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //make this into a switch statement
+
+  if (alive) {
+    switch (guess) {
+      case 0:
+        changeMessage('No Number!!');
+        break;
+      case secretNumber:
+        changeBG('hsl(200 50% 50%)');
+        document.querySelector('.number').style.width = '30rem';
+        changeMessage('Correct!!!');
+        document.querySelector('.number').textContent = secretNumber;
+        if (score > highScore) {
+          highScore = score;
+          document.querySelector('.highscore').textContent = highScore;
+        }
+        break;
+      default:
+        if (score > 1) {
+          changeMessage(guess > secretNumber ? 'Too High!!' : 'Too Low');
+          score--;
+        } else {
+          changeMessage('you lost');
+          document.querySelector('.number').textContent = ':(';
+          alive = false;
+          changeBG('red');
+        }
+        document.querySelector('.score').textContent = score;
+        break;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => initGame());
